0f317c6aee6b56cbd5c1dbeb3577b148f9e92a3f
Kevin573 <kevinochieng932@gmail.com>
2021-03-20 14:28:31 +0300

    added ad  section

--- src/components/Brand/Brand.jsx
+++ src/components/Brand/Brand.jsx
@@ -3,17 +3,13 @@ import './Brand.css';
 
 const Brand = () => {
   return (
-    <div className='brand'>
-      <div className='small-container'>
-        <div className='row'>
-          <a href='https://www.bluehost.com/track/kevin932/' target='_blank'>
-            <img
-              border='0'
-              src='https://bluehost-cdn.com/media/partner/images/kevin932/760x80/760x80BW.png'
-            />
-          </a>
-        </div>
-      </div>
+    <div className='ad-section'>
+      <a href='https://www.bluehost.com/track/kevin932/' target='_blank'>
+        <img
+          border='0'
+          src='https://bluehost-cdn.com/media/partner/images/kevin932/760x80/760x80BW.png'
+        />
+      </a>
     </div>
   );
 };
